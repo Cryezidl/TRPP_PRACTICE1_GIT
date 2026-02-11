30538f530ca21351322ff27733daf2920a1b7503
IamOnla1n <21tim05@mail.ru>
2026-02-11 09:30:09 +0300

    fix

--- examples/array.py
+++ examples/array.py
@@ -1,15 +1,18 @@
 arr = list()
 n = int(input("Enter number of elements: "))
-print("Enter elements:")
-i = 0
-while (i < n):
-    tmp = int(input(""))
-    arr.append(tmp)
-    i += 1
+if n > 0 and n <= 15:
 
-print("Array:")
-i = 0
-while (i < n):
-    print(arr[i], end = " ")
-    i += 1
+
+    print("Enter elements:")
+    i = 0
+    while (i < n):
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+
+    print("Array:")
+    i = 0
+    while (i < n):
+        print(arr[i], end = " ")
+        i += 1
 print("")
